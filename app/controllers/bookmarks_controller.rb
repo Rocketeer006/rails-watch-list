4851db34191516921eb0d3b659cb7ad7690ceaa7
Adam Kerr <sirpoy@hotmail.com>
2021-11-12 11:10:35 +0100

    updates to lists

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,23 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmarks = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render "new"
+    end
+  end
+end
